a9e4069e02b89b420acbc0c40c87bcb52ce97750
Arshad Danish <arshaddanishthana@gmail.com>
2022-06-29 20:18:37 +0530

    integrate add-task

--- frontend/src/pages/add-task/AddTask.js
+++ frontend/src/pages/add-task/AddTask.js
@@ -1,24 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import apis from "../../apis";
 
 export default function AddTask() {
+  const navigate = useNavigate();
+
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  let onFormSubmit = async (e) => {
+    e.preventDefault();
+    await apis.post("/tasks", { text: text, day: day, reminder: reminder });
+    navigate("/");
+  };
+
   return (
-    <div className="mt-5">
+    <div className="mt-5" onSubmit={onFormSubmit}>
       <form action="">
         <div class="mb-4">
           <label class="form-label">Text</label>
-          <input type="text" class="form-control" placeholder="Text" />
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
         </div>
         <div class="mb-4">
           <label class="form-label">Day</label>
-          <input type="text" class="form-control" placeholder="Day" />
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Day"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+          />
         </div>
         <div class="form-check mb-4">
-          <input class="form-check-input" type="checkbox" />
+          <input
+            class="form-check-input"
+            type="checkbox"
+            value={reminder}
+            onChange={() => setReminder(!reminder)}
+          />
           <label class="form-check-label">Set Reminder</label>
         </div>
         <div className="text-center mb-4">
-          <button className="btn btn-success add-task-btn">Add Task</button>
+          <button type="submit" className="btn btn-success add-task-btn">
+            Add Task
+          </button>
         </div>
       </form>
 
