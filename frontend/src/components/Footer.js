1131b826876296a61764d2edcfaabd3074c70e9c
Arshad Danish <arshaddanishthana@gmail.com>
2022-06-29 19:58:22 +0530

    go back route

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="mt-5">
-      <p className="text-center">&copy; Copyright CS 2K19</p>
+      <p className="text-center">
+        &copy; Copyright <Link to="/about">CS 2K19</Link>
+      </p>
     </div>
   );
 }
--- frontend/src/pages/add-task/AddTask.js
+++ frontend/src/pages/add-task/AddTask.js
@@ -1,26 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function AddTask() {
   return (
     <div className="mt-5">
-      <div class="mb-4">
-        <label class="form-label">Text</label>
-        <input type="text" class="form-control" placeholder="Text" />
-      </div>
+      <form action="">
+        <div class="mb-4">
+          <label class="form-label">Text</label>
+          <input type="text" class="form-control" placeholder="Text" />
+        </div>
+        <div class="mb-4">
+          <label class="form-label">Day</label>
+          <input type="text" class="form-control" placeholder="Day" />
+        </div>
+        <div class="form-check mb-4">
+          <input class="form-check-input" type="checkbox" />
+          <label class="form-check-label">Set Reminder</label>
+        </div>
+        <div className="text-center mb-4">
+          <button className="btn btn-success add-task-btn">Add Task</button>
+        </div>
+      </form>
 
-      <div class="mb-4">
-        <label class="form-label">Day</label>
-        <input type="text" class="form-control" placeholder="Day" />
-      </div>
-
-      <div class="form-check mb-4">
-        <input class="form-check-input" type="checkbox" />
-        <label class="form-check-label">Set Reminder</label>
-      </div>
-
-      <div className="text-center">
-        <button className="btn btn-success add-task-btn">Add Task</button>
-      </div>
+      <Link to="/">Go Back</Link>
     </div>
   );
 }
