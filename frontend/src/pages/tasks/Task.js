ff0286cb84ea09e979926bd2ac967e5ca3b2dc5d
Arshad Danish <arshaddanishthana@gmail.com>
2022-06-29 19:50:02 +0530

    add reminder style

--- frontend/src/pages/tasks/Task.js
+++ frontend/src/pages/tasks/Task.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Task({ task }) {
   return (
-    <div className="task">
+    <div className={`task ${task.reminder ? "reminder" : ""}`}>
       <div>
         <p>{task.text}</p>
         <p>{task.day}</p>
