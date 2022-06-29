44a781a911f38f872fb115d02cd57c66a9a33bea
Arshad Danish <arshaddanishthana@gmail.com>
2022-06-29 22:17:41 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(express.static("build"));
+
 // Import Routers | APIs
 const TasksRouter = require("./routes/tasks.js");
 app.use(TasksRouter);
@@ -10,7 +15,7 @@ const port = 9000;
 
 // Serve React Build
 app.get("/", (req, res) => {
-  res.send("I am the React App Build");
+  res.sendFile("/index.html");
 });
 
 // Another Route | API
