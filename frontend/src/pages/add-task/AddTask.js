59638e0462c5c15f97733806dac9f62d2eb528f6
Arshad Danish <arshaddanishthana@gmail.com>
2022-07-04 15:57:19 +0530

    uhikg

--- frontend/src/pages/add-task/AddTask.js
+++ frontend/src/pages/add-task/AddTask.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function AddTask() {
+  let [text, setText] = useState("");
+  let [day, setDay] = useState("");
+  let [reminder, setReminder] = useState(false);
+
+  let onFormSubmit = (e) => {
+    e.preventDefault();
+    let newTask = {
+      text: text,
+      day: day,
+      reminder: reminder,
+    };
+    console.log(newTask);
+  };
+
   return (
     <div className="mt-5">
-      <div class="mb-4">
-        <label class="form-label">Text</label>
-        <input type="text" class="form-control" placeholder="Text" />
-      </div>
+      <form onSubmit={onFormSubmit}>
+        <div className="mb-4">
+          <label className="form-label">Text</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Text"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="form-label">Day</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Day"
+            onChange={(e) => setDay(e.target.value)}
+            value={day}
+          />
+        </div>
+        <div className="form-check mb-4">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onChange={() => setReminder(!reminder)}
+            value={reminder}
+          />
+          <label className="form-check-label">Set Reminder</label>
+        </div>
+        <div className="text-center">
+          <button type="submit" className="btn btn-success add-task-btn">
+            Add Task
+          </button>
+        </div>
+      </form>
 
-      <div class="mb-4">
-        <label class="form-label">Day</label>
-        <input type="text" class="form-control" placeholder="Day" />
-      </div>
-
-      <div class="form-check mb-4">
-        <input class="form-check-input" type="checkbox" />
-        <label class="form-check-label">Set Reminder</label>
-      </div>
-
-      <div className="text-center">
-        <button className="btn btn-success add-task-btn">Add Task</button>
-      </div>
+      <Link to="/">
+        <p className="mt-5 text-center">Back To Home</p>
+      </Link>
     </div>
   );
 }
